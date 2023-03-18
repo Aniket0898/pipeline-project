a8b5a40f4e589c4318a30a6a7202b9051caee7fa
Aniket Patil <68344331+Aniket0898@users.noreply.github.com>
2023-03-19 01:03:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,12 @@ provider "aws" {
   secret_key = "DAtijEOVADSqSARMJwHqx6DQdecQDG72gYWkaGD4"
 }
 
+data "aws_vpc" "demo_app" {
+  tags = {
+    Name = "demo_app"
+  }
+}
+
 resource "aws_ecr_repository" "demo_app" {
   name                 = "demo_app"
   image_tag_mutability = "MUTABLE"
@@ -15,37 +21,15 @@ resource "aws_ecs_cluster" "demo_app" {
   capacity_providers = ["FARGATE"]
 }
 
-resource "aws_iam_role" "ecs_execution_role" {
-  name = "ecs-execution-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com"
-        }
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "ecs_execution_role_attachment" {
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-  role       = aws_iam_role.ecs_execution_role.name
-}
-
 resource "aws_ecs_task_definition" "demo_app" {
   family                   = "demo_app"
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
   cpu                      = "256"
   memory                   = "512"
-  execution_role_arn       = aws_iam_role.ecs_execution_role.arn
   container_definitions    = jsonencode([{
     name      = "demo_app"
-    image     = "622696765016.dkr.ecr.ap-south-1.amazonaws.com/demo_app:latest"
+    image     = "${aws_ecr_repository.demo_app.repository_url}:latest"
     portMappings = [{
       containerPort = 3000
       protocol      = "tcp"
@@ -60,17 +44,6 @@ resource "aws_vpc" "demo_app" {
   }
 }
 
-resource "aws_security_group" "demo_app" {
-  name_prefix = "demo_app_sg_"
-
-  ingress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
 variable "availability_zones" {
   type    = list(string)
   default = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
@@ -79,9 +52,21 @@ variable "availability_zones" {
 resource "aws_subnet" "demo_app" {
   count = length(var.availability_zones)
 
-  cidr_block        = "10.0.${count.index + 1}.0/24"
+  cidr_block = "10.0.${count.index + 1}.0/24"
   availability_zone = "${var.availability_zones[count.index]}"
-  vpc_id            = aws_vpc.demo_app.id
+  vpc_id     = data.aws_vpc.demo_app.id
+}
+
+resource "aws_security_group" "demo_app" {
+  name_prefix = "demo_app_sg_"
+  vpc_id      = data.aws_vpc.demo_app.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_ecs_service" "demo_app" {
