7eedfedb67616c0a2b6510d4d307f4f38aa2dbf1
Aniket Patil <68344331+Aniket0898@users.noreply.github.com>
2023-03-18 23:16:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,12 @@
-aws_access_key_id = "${{ secrets.AWS_ACCESS_KEY_ID }}"
-aws_secret_access_key = "${{ secrets.AWS_SECRET_ACCESS_KEY }}"
-
 provider "aws" {
   region = "ap-south-1"
   access_key = var.aws_access_key_id
   secret_key = var.aws_secret_access_key
+}
+
+variable "aws_access_key_id" {}
+
+variable "aws_secret_access_key" {}
 }
 
 resource "aws_ecr_repository" "demo_app" {
