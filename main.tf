b4ad32f701ff68e8ca7f9ec077a876017c3080e4
Aniket Patil <68344331+Aniket0898@users.noreply.github.com>
2023-03-19 15:37:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,72 +4,37 @@ provider "aws" {
   secret_key = "DAtijEOVADSqSARMJwHqx6DQdecQDG72gYWkaGD4"
 }
 
-data "aws_vpc" "demo_app" {
-  filter {
-    name   = "tag:Name"
-    values = ["demo_app"]
-  }
-
-  filter {
-    name   = "state"
-    values = ["available"]
-  }
-
-  depends_on = [aws_ecr_repository.demo_app]
-}
-
-resource "aws_ecr_repository" "demo_app" {
-  name                 = "demo_app"
-  image_tag_mutability = "MUTABLE"
-}
-
-resource "aws_ecs_cluster" "demo_app" {
-  name = "demo_app"
-
-  capacity_providers = ["FARGATE"]
-}
-
-resource "aws_ecs_task_definition" "demo_app" {
-  family                   = "demo_app"
-  requires_compatibilities = ["FARGATE"]
-  network_mode             = "awsvpc"
-  cpu                      = "256"
-  memory                   = "512"
-  container_definitions    = jsonencode([{
-    name      = "demo_app"
-    image     = "${aws_ecr_repository.demo_app.repository_url}:latest"
-    portMappings = [{
-      containerPort = 3000
-      protocol      = "tcp"
-    }]
-  }])
-}
-
-resource "aws_vpc" "demo_app" {
+resource "aws_vpc" "demoapp" {
   cidr_block = "10.0.0.0/16"
   tags = {
-    Name = "demo_app"
+    Name = "demoapp-vpc"
   }
 }
 
-variable "availability_zones" {
-  type    = list(string)
-  default = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
+resource "aws_subnet" "demoapp_public_1" {
+  vpc_id     = "${aws_vpc.demoapp.id}"
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-west-2a"
 }
 
-resource "aws_subnet" "demo_app" {
-  count = length(var.availability_zones)
-
-  cidr_block = "10.0.${count.index + 1}.0/24"
-  availability_zone = "${var.availability_zones[count.index]}"
-  vpc_id     = data.aws_vpc.demo_app.id
+resource "aws_subnet" "demoapp_public_2" {
+  vpc_id     = "${aws_vpc.demoapp.id}"
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-west-2b"
 }
 
-resource "aws_security_group" "demo_app" {
-  name_prefix = "demo_app_sg_"
-  vpc_id      = data.aws_vpc.demo_app.id
+resource "aws_security_group" "ecs_tasks" {
+  name_prefix = "ecs-tasks-"
+  vpc_id      = "${aws_vpc.demoapp.id}"
 
   ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/8"]
+  }
+
+  egress {
     from_port   = 0
     to_port     = 65535
     protocol    = "tcp"
@@ -77,14 +42,50 @@ resource "aws_security_group" "demo_app" {
   }
 }
 
-resource "aws_ecs_service" "demo_app" {
-  name            = "demo_app"
-  cluster         = aws_ecs_cluster.demo_app.id
-  task_definition = aws_ecs_task_definition.demo_app.arn
+resource "aws_ecr_repository" "demoapp" {
+  name = "demoapp"
+}
+
+resource "aws_ecs_task_definition" "demoapp" {
+  family                   = "demoapp"
+  container_definitions    = <<DEFINITION
+  [
+    {
+      "name": "demoapp",
+      "image": "${aws_ecr_repository.demoapp.repository_url}",
+      "cpu": 256,
+      "memory": 512,
+      "essential": true,
+      "portMappings": [
+        {
+          "containerPort": 3000,
+          "hostPort": 3000
+        }
+      ]
+    }
+  ]
+  DEFINITION
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+}
+
+
+resource "aws_ecs_cluster" "demoapp" {
+  name = "demoapp"
+}
+
+resource "aws_ecs_service" "demoapp" {
+  name            = "demoapp"
+  cluster         = "${aws_ecs_cluster.demoapp.id}"
+  task_definition = "${aws_ecs_task_definition.demoapp.arn}"
   desired_count   = 1
   launch_type     = "FARGATE"
+
   network_configuration {
-    security_groups = [aws_security_group.demo_app.id]
-    subnets         = aws_subnet.demo_app.*.id
+    subnets          = ["${aws_subnet.demoapp_public_1.id}", "${aws_subnet.demoapp_public_2.id}"]
+    security_groups  = ["${aws_security_group.ecs_tasks.id}"]
+    assign_public_ip = "ENABLED"
   }
-}
+
+  load_balancer {
+    target_group_arn = "${aws_lb_target_group.demoapp.arn}"
