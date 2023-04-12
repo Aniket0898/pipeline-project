4517c8eb0f5f6ee9e89cd86e5147ea4e8927b682
Aniket Patil <68344331+Aniket0898@users.noreply.github.com>
2023-04-12 15:02:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -51,6 +51,16 @@ resource "aws_cloudwatch_log_group" "ecs_logs" {
   }
 } 
 
+resource "aws_cloudwatch_log_stream" "ecs_logs_demo_container" {
+  name           = "demo_container"
+  log_group_name = aws_cloudwatch_log_group.ecs_logs.name
+}
+
+  tags = {
+    Name = "ecs_logs"
+  }
+} 
+
 resource "aws_ecr_repository" "demo_repo" {
   name = "demo_repo"
 }
