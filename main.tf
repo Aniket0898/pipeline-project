6d4b27c118fb2006ed9cf5afa22ea59ec9fff0d7
Aniket Patil <68344331+Aniket0898@users.noreply.github.com>
2023-04-12 17:09:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -110,9 +110,9 @@ resource "aws_ecs_task_definition" "taskdefinition" {
     memory                   = "3072"
     network_mode             = "awsvpc"
     execution_role_arn       = aws_iam_role.demo_app_task_execution_role.arn
-    task_role_arn            = aws_iam_role.demo_app_task_execution_role.arn
-      }
-    ])
+    task_role_arn            = aws_iam_role.demo_app_task_execution_role.arn   
+    }
+   ])
   }
 
 resource "aws_iam_role" "demo_app_task_execution_role" {
